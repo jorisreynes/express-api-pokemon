3f871d79dfbc665903acc518e37b9987ac7e4d44
Najib Harribou <najib.harribou@campus-igs-toulouse.fr>
2023-01-23 11:10:51 +0100

    model fix

--- backend/models/pokemonModel.js
+++ backend/models/pokemonModel.js
@@ -1,77 +1,77 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+
+
+const typeSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+  },
+});
 
 const pokemonSchema = mongoose.Schema(
-    {
-        idPokedex:{
-            type:Number,
-            required: [true, 'Please add a value']
-        },
-        name: {
-            type: String,
-            required: [true, 'Please add a text value']
-        },
-        height:{
-            type:Number,
-            required: [true, 'Please add a value']
-        },
-        weight:{
-            type:Number,
-            required: [true, 'Please add a value']
-        },
-        statistiques:{
-            type:String,
-            required: [true, 'Please add a value']
-        },
-        type:{
-            type:typeSchema,
-            required: [true, 'please add a type']
-        }
-    }, 
-    {
-        timestamps: true
-    }
-)
+  {
+    idPokedex: {
+      type: Number,
+      required: [true, "Please add a value"],
+    },
+    name: {
+      type: String,
+      required: [true, "Please add a text value"],
+    },
+    height: {
+      type: Number,
+      required: [true, "Please add a value"],
+    },
+    weight: {
+      type: Number,
+      required: [true, "Please add a value"],
+    },
+    statistiques: {
+      type: String,
+      required: [true, "Please add a value"],
+    },
+    type: {
+      type: typeSchema,
+      required: [true, "please add a type"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const competenceSchema = mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: [true, 'Please add a name']
-        },
-        description:{
-            type:Number,
-            required: [true, 'Please add a value']
-        },
-        power:{
-            type:Number,
-            required: [true, 'Please add a value']
-        },
-        precision:{
-            type:Number,
-            required: [true, 'Please add a value']
-        },
-        ppMax:{
-            type:Number,
-            required: [true, 'Please add a value']
-        },
-        type:{
-            type:typeSchema,
-            required: [true, 'please add a type']
-        }
-        
+const competenceSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+  },
+  description: {
+    type: Number,
+    required: [true, "Please add a value"],
+  },
+  power: {
+    type: Number,
+    required: [true, "Please add a value"],
+  },
+  precision: {
+    type: Number,
+    required: [true, "Please add a value"],
+  },
+  ppMax: {
+    type: Number,
+    required: [true, "Please add a value"],
+  },
+  type: {
+    type: typeSchema,
+    required: [true, "please add a type"],
+  },
+});
 
-    }
-)
-
-const typeSchema = mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: [true, 'Please add a name']
-        }
-        
-    }
-)
-
-
-module.exports = mongoose.model('Pokemon', pokemonSchema, 'Competences', competenceSchema, 'Type', typeSchema)
+module.exports = mongoose.model(
+  "Pokemon",
+  pokemonSchema,
+  "Competences",
+  competenceSchema,
+  "Type",
+  typeSchema
+);
