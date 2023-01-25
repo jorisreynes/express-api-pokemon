f11fb934f616f7c26a9b119911a6c74efab6407d
Antonio CAMARILLO ABAD <acabad02@gmail.com>
2023-01-25 10:09:04 +0100

    mis à jour de méthode get all et get pour obtenir le type des pokémons

--- backend/controllers/pokemonController.js
+++ backend/controllers/pokemonController.js
@@ -5,13 +5,22 @@ const Pokemon = require("../models/pokemonModel");
 const Type = require("../models/typesModel");
 
 const getPokemons = asyncHandler(async (req, res) => {
+  
   let pokemons = await Pokemon.find({});
+
+  for (const pokemon in pokemons) {
+    let type = await Type.findById(pokemons[pokemon].type);
+    pokemons[pokemon].type = type; 
+    //console.log(`${pokemons[pokemon]} ${type}`);
+  }
   res.status(200).json(pokemons);
 });
 
 const getPokemon = asyncHandler(async (req, res) => {
   const pokemon = await Pokemon.findById(req.params.id);
+  let type = await Type.findById(pokemon.type);
   if (pokemon) {
+    pokemon.type = type;
     res.status(200).json(pokemon);
   } else {
     res.status(404);
@@ -55,9 +64,6 @@ const updatePokemon = asyncHandler(async (req, res) => {
 
   res.status(200).json(pokemon);
 });
-
-
-
 
 const deletePokemon = asyncHandler(async (req, res) => {
   const pokemon = await Pokemon.findById(req.params.id);
